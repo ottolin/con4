be675e9133a834801100689ba4ef5ef179d09050
Otto Lin <ottolin@gmail.com>
2021-01-07 17:26:54 +0800

    Adding tests, moving init code to constructor

--- src/con4.cpp
+++ src/con4.cpp
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <assert.h>
 #include "con4.hpp"
+
+#define DEBUG //printf
 
 struct GameState {
   char board[BOARD_WIDTH * BOARD_HEIGHT];
   int nextIndex[BOARD_WIDTH];
+  int lastFilledPos;
   int state;
   int step;
-};
+  int player;
 
-#define DEBUG //printf
+  GameState()
+    :lastFilledPos(-1)
+     ,state(GAME_CONTINUE)
+     ,step(0)
+     ,player(-1) {
+       memset(board, B_EMPTY, sizeof(board));
+       for(int i=0; i< BOARD_WIDTH; i++) {
+         nextIndex[i] = PADDING;
+       }
+     }
+};
 
 void printFullBoard(char (&board)[BOARD_SIZE]) {
   for(int i = BOARD_HEIGHT-1; i>= 0; i--) {
@@ -39,7 +53,8 @@ int validateInput(int inputColumn) {
   }
 }
 
-void fillColumn(GameState &game, int inputColumn, int player, int &filledPosition) {
+// filling up the board with user input
+void fillColumn(GameState &game, int inputColumn) {
   int i = game.nextIndex[inputColumn];
 
   if (i >= GAME_HEIGHT + PADDING) {
@@ -48,10 +63,10 @@ void fillColumn(GameState &game, int inputColumn, int player, int &filledPositio
     int index =  i * BOARD_WIDTH + inputColumn + PADDING;
     char target = game.board[index];
     if (target == B_EMPTY) {
-      game.board[ i * BOARD_WIDTH + inputColumn + PADDING ] = player;
+      game.board[ i * BOARD_WIDTH + inputColumn + PADDING ] = game.player;
       game.nextIndex[inputColumn]++;
       game.state = GAME_CONTINUE;
-      filledPosition = index;
+      game.lastFilledPos = index;
     } else {
       // shouldnt happen. anyway.
       game.state = GAME_ERROR_INPUT;
@@ -59,7 +74,9 @@ void fillColumn(GameState &game, int inputColumn, int player, int &filledPositio
   }
 }
 
-void checkVertical(GameState &game, int position) {
+// Check the only vertical winning condition
+void checkVertical(GameState &game) {
+  int position = game.lastFilledPos;
   char target = game.board[position];
   if ((target == game.board[position])
       && (target == game.board[position - BOARD_WIDTH])
@@ -70,11 +87,11 @@ void checkVertical(GameState &game, int position) {
   }
 }
 
-void checkHorizontal(GameState &game, int position) {
+// Check horizontal winning conditions
+void checkHorizontal(GameState &game) {
   bool isHoriConnected = false;
-
   auto isConnected = [=](int offset) {
-    int startPos = position - offset;
+    int startPos = game.lastFilledPos - offset;
     char target = game.board[startPos];
     if ((target == game.board[startPos + 1])
         && (target == game.board[startPos + 2])
@@ -95,10 +112,12 @@ void checkHorizontal(GameState &game, int position) {
   }
 }
 
-void checkDiagonals(GameState &game, int position) {
+// Check diagonal winning conditions
+// for both positive and negative slope
+void checkDiagonals(GameState &game) {
   bool isDiagConnected = false;
   auto isConnected = [=](int offset, int stepSize) {
-    int startPos = position + offset;
+    int startPos = game.lastFilledPos + offset;
     char target = game.board[startPos];
     if ((target == game.board[startPos - stepSize])
         && (target == game.board[startPos - 2 * stepSize])
@@ -121,19 +140,20 @@ void checkDiagonals(GameState &game, int position) {
   }
 }
 
-void checkConnectionAtPosition(GameState &game, int position) {
+void checkConnections(GameState &game) {
   do
   {
-    checkVertical(game, position);
+    checkVertical(game);
     if (game.state == GAME_WON) break;
-    checkHorizontal(game, position);
+    checkHorizontal(game);
     if (game.state == GAME_WON) break;
-    checkDiagonals(game, position);
+    checkDiagonals(game);
   }while(0);
 }
 
-void runGame(GameState &game, int inputColumn, int player) {
-  int pos = -1;
+void runGame(GameState &game, int inputColumn) {
+  game.player = (game.player + 1) % 2;
+  game.step++;
 
   do
   {
@@ -143,31 +163,29 @@ void runGame(GameState &game, int inputColumn, int player) {
       break;
     }
 
-    fillColumn(game, inputColumn, player, pos);
-    DEBUG("Filled column %d (Pos: board[%d])\n", inputColumn, pos);
+    fillColumn(game, inputColumn);
+    DEBUG("Filled column %d (Pos: board[%d])\n", inputColumn, game.lastFilledPos);
     if (game.state != GAME_CONTINUE) break;
 
-    checkConnectionAtPosition(game, pos);
+    checkConnections(game);
     printBoard(game.board);
     //printFullBoard(board);
 
   } while(0);
+
+  if (game.state == GAME_CONTINUE && game.step >= GAME_SIZE) {
+    game.state = GAME_DRAW;
+  }
 }
 
 void gameLoop(GameState &game) {
   int32_t input = 0;
-  int player = -1;
   while(game.state == GAME_CONTINUE) {
     DEBUG("Gameloop step %u\n", game.step);
-    player = (player + 1) % 2;
 
     if (scanf(" %d", &input) > 0) {
       DEBUG("Gameloop step %u, input: %d\n", game.step, input);
-      runGame(game, input, player);
-      game.step++;
-      if (game.step >= GAME_SIZE) {
-        game.state = GAME_DRAW;
-      }
+      runGame(game, input);
     } else {
       // input fail
       game.state = GAME_ERROR_INPUT;
@@ -180,7 +198,7 @@ void gameLoop(GameState &game) {
       printf("DRAW\n");
       break;
     case GAME_WON:
-      printf("WINNER: %s\n", player ? "Player 2" : "Player 1");
+      printf("WINNER: %s\n", game.player ? "Player 2" : "Player 1");
       break;
     case GAME_ERROR_INPUT:
       DEBUG("Error input\n");
@@ -191,19 +209,89 @@ void gameLoop(GameState &game) {
   }
 }
 
-void initState(GameState &game) {
-  memset(game.board, B_EMPTY, sizeof(game.board));
-  game.state = GAME_CONTINUE;
-  game.step = 0;
+////////////////////////
+// Tests
+//
 
-  for(int i=0; i< BOARD_WIDTH; i++) {
-    game.nextIndex[i] = PADDING;
+void test01() {
+  GameState game;
+  int input[] = { 0, 1, 0, 1, 0, 1, 0 };
+  for (int i=0; i<7; i++) {
+    runGame(game, input[i]);
   }
+  assert(game.state == GAME_WON);
+  assert(game.player == 0);
 }
 
-int main(int argc, char** argv) {
+void test02() {
   GameState game;
-  initState(game);
-  gameLoop(game);
+  int input[] = { 0,0,0,1,1,2,1,2,2,3,3,3,3 };
+  for (int i=0; i<13; i++) {
+    runGame(game, input[i]);
+  }
+  assert(game.state == GAME_WON);
+  assert(game.player == 0);
+}
+
+void test03() {
+  GameState game;
+  int input[] = { 0,0,1,1,2,2,3 };
+  for (int i=0; i<7; i++) {
+    runGame(game, input[i]);
+  }
+  assert(game.state == GAME_WON);
+  assert(game.player == 0);
+}
+
+void test04() {
+  GameState game;
+  int input[] = { 0,1,3,0,1,0,0,0,1,2,2 };
+  for (int i=0; i<11; i++) {
+    runGame(game, input[i]);
+  }
+  assert(game.state == GAME_WON);
+  assert(game.player == 0);
+}
+
+void test05() {
+  // invalid input test
+  GameState game;
+  int input[] = { 0,1,3,9 };
+  for (int i=0; i<4; i++) {
+    runGame(game, input[i]);
+  }
+  assert(game.state == GAME_ERROR_INPUT);
+}
+
+void test06() {
+  // draw test
+  GameState game;
+  int input[] = { 0,1,0,1,0,1,1,0,1,0,1,0,2,3,2,3,2,3,3,2,3,2,3,2,4,5,4,5,4,5,5,4,5,4,5,4,6,6,6,6,6,6 };
+  for (int i=0; i<42; i++) {
+    runGame(game, input[i]);
+  }
+  assert(game.state == GAME_DRAW);
+}
+
+void runTest() {
+  test01();
+  test02();
+  test03();
+  test04();
+  test05();
+  test06();
+}
+
+////////////////////////
+// Entry
+//
+int main(int argc, char** argv) {
+  if (argc == 1) {
+    GameState game;
+    gameLoop(game);
+  } else {
+    runTest();
+  }
   return 0;
 }
+
